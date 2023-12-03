980c091cc68a1bf19e61bbfc72bd3f1481242d17
yung-slime <snakescitybigstateindia@gmail.com>
2023-12-03 14:38:07 +0530

    ready to go

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { Button } from '@/components/ui/button'
 import React, { Children } from 'react'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
 import './globals.css'
 import { ThemeProvider } from "@/contexts/ThemeContext"
 
 const App = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      {children}
+      <div className='text-[48px]'>WHTAA</div>
     </ThemeProvider>
   )
 }
